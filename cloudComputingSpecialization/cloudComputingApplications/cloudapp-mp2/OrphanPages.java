4b07359677af9b441d49245ed916db2fc2ad7612
Aleix Penella <aleix.penella@gmail.com>
2015-09-16 07:13:40 +0000

    cloudapp-mp2: OrphanPages done

--- cloudComputingSpecialization/cloudComputingApplications/cloudapp-mp2/OrphanPages.java
+++ cloudComputingSpecialization/cloudComputingApplications/cloudapp-mp2/OrphanPages.java
@@ -2,6 +2,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.ArrayWritable;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -9,11 +10,21 @@ import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.FSDataInputStream;
 
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
 // >>> Don't Change
 public class OrphanPages extends Configured implements Tool {
@@ -27,9 +38,13 @@ public class OrphanPages extends Configured implements Tool {
     @Override
     public int run(String[] args) throws Exception {
     	//TODO: Aleix Penella
+	Configuration conf = this.getConf();
 	FileSystem fs = FileSystem.get(conf);
         Path tmpPath = new Path("/mp2/tmp");
         fs.delete(tmpPath, true);
+
+	//delete the output folder
+        fs.delete(new Path(args[1]), true);
 
 	Job jobA = Job.getInstance(this.getConf(), "Link Count");
 	jobA.setOutputKeyClass(IntWritable.class);
@@ -46,11 +61,11 @@ public class OrphanPages extends Configured implements Tool {
 
 	Job jobB = Job.getInstance(conf, "Orphan Pages");
         jobB.setOutputKeyClass(IntWritable.class);
-        jobB.setOutputValueClass(NullWritable.class);
+        jobB.setOutputValueClass(IntWritable.class);
 
 	//todo
 	jobB.setMapOutputKeyClass(IntWritable.class);
-        jobB.setMapOutputValueClass(IntArrayWritable.class);
+        jobB.setMapOutputValueClass(IntWritable.class);
 	
 	jobB.setMapperClass(OrphanPagesMap.class);
         jobB.setReducerClass(OrphanPagesReduce.class);
@@ -81,6 +96,17 @@ public class OrphanPages extends Configured implements Tool {
         return everything.toString();
     }
 
+    public class IntArrayWritable extends ArrayWritable{
+        
+        public IntArrayWritable() {
+        	super(IntWritable.class);
+    	}
+        
+    	public IntArrayWritable(IntWritable[] values) {
+        	super(IntWritable.class, values);
+    	}
+    }
+
 	/*
 		Strategy: 
 		jobA:
@@ -95,33 +121,71 @@ public class OrphanPages extends Configured implements Tool {
 
     // JobA: Map count link
     public static class LinkCountMap extends Mapper<Object, Text, IntWritable, IntWritable> {
+
+	@Override
+        protected void setup(Context context) throws IOException,InterruptedException {
+            Configuration conf = context.getConfiguration();
+	}
+
         @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
         	//TODO: Aleix Penella
+		String token = new String();
+		IntWritable one = new IntWritable(1);
+		IntWritable zero = new IntWritable(0);
 		
+		StringTokenizer pageLinks = new StringTokenizer(value.toString(),":");
+		//get the page id
+		if (pageLinks.hasMoreTokens()){
+			context.write(new IntWritable(Integer.parseInt(pageLinks.nextToken())),zero);
+			// get the page's links 	
+			if (pageLinks.hasMoreTokens()){
+				StringTokenizer links = new StringTokenizer(pageLinks.nextToken());
+				while (links.hasMoreTokens()) {
+					context.write(new IntWritable(Integer.parseInt(links.nextToken())),one);
+     				}
+			}
+		}	
         }
     }
     // JobA: Reduce count link
-    public static class LinkCountReduce extends Mapper<IntWritable, IntWritable, IntWritable, IntWritable> {
-        @Override
-        public void reduce(IntWritable key, IntWritable value, Context context) throws IOException, InterruptedException {
+    public static class LinkCountReduce extends Reducer<IntWritable, IntWritable, IntWritable, IntWritable> {
+
+	@Override
+        public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
         	//TODO: Aleix Penella
+		int sum = 0;
+		for ( IntWritable link: values ){
+			sum += link.get(); 
+		}
+		context.write(new IntWritable(sum),key);	
         }
     }
 
 
     // JobB: Map OrphanPages 
-    public static class OrphanPagesMap extends Reducer<IntWritable, IntArrayWritable, IntWritable, NullWritable> {
+    public static class OrphanPagesMap extends Mapper<Text, Text, IntWritable, IntWritable> {
+
+	@Override
+        protected void setup(Context context) throws IOException,InterruptedException {
+            Configuration conf = context.getConfiguration();
+	}
+
         @Override
-        public void map(IntWritable key, Iterable<IntArrayWritable> values, Context context) throws IOException, InterruptedException {
+        public void map(Text key, Text values, Context context) throws IOException, InterruptedException {
         	//TODO: Aleix Penella
+		if (new IntWritable(Integer.parseInt(key.toString())).equals(new IntWritable(0))){
+			context.write(new IntWritable(Integer.parseInt(values.toString())), new IntWritable(Integer.parseInt(key.toString())));
+		}	
         }
     }
     // JobB: Reduce OrphanPages
     public static class OrphanPagesReduce extends Reducer<IntWritable, IntWritable, IntWritable, NullWritable> {
-        @Override
+        
+	@Override
         public void reduce(IntWritable key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
         	//TODO: Aleix Penella
+		context.write(key, NullWritable.get());
         }
     }
 }
